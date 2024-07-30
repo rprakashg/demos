44ee3cc86a9589a2f28095055c2ce51722e9b132
Ram Gopinathan <rprakashg@gmail.com>
2024-07-29 22:10:52 -0700

    Minor edits and other cleanups

--- plugins/modules/install_openshift_on_aws.py
+++ plugins/modules/install_openshift_on_aws.py
@@ -5,13 +5,6 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
-
-ANSIBLE_METADATA = {
-    "metadata_version": "0.1",
-    "status": ["preview"],
-    "supported_by": "Community"
-}
-
 DOCUMENTATION = r'''
 ---
 module: install openshift on aws
