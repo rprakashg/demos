0b08f1fef13b943442a21bbabc5197df4a51196a
Ram Gopinathan <rprakashg@gmail.com>
2024-08-03 14:37:14 -0700

    Testing

--- plugins/modules/install_openshift_on_aws.py
+++ plugins/modules/install_openshift_on_aws.py
@@ -106,7 +106,8 @@ def get_azs(region, replicas):
     return azs[:take]
 
 def generate_installconfig_template():
-    template="""apiVersion: v1
+    template="""
+    apiVersion: v1
     baseDomain: {{ base_domain }}
     compute:
     - architecture: amd64
