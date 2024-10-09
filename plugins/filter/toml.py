4fb056fa090c703a837729ba4d1e52e0e95ec282
Ram Gopinathan <rprakashg@gmail.com>
2024-10-09 10:36:10 -0700

    added try catch

--- plugins/filter/toml.py
+++ plugins/filter/toml.py
@@ -5,10 +5,15 @@ from ansible.module_utils._text import to_text
 import toml
 
 def load_toml_file(file):
-    with open(file, 'r') as f:
-        data = toml.load(f)
-    return data
-
+    try:
+        with open(file, 'r') as f:
+            data = toml.load(f)
+        return data
+    except: 
+        raise AnsibleRuntimeError(
+            'Error loading "toml" file. Please check the file'        
+        )
+    
 def from_toml(toml_file):
     if not isinstance(toml_file, string_types):
         raise AnsibleFilterError('from_toml requires a string, got %s' % type(0))
